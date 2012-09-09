a557ed0de87fe14b3027952feb8e69d7a548175b
Manfred Klaffenboeck <fred_klaf@gmx.net>
2012-09-09 23:00:24 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HappyMandiCms::Application.config.secret_token = '6baceefa911a5cfbdf79c531d411d97f17eecd6f660d4178226f450456ecb19459974dcc4aab8c9a11aab0465e92403cd28cf503577e6a6e0fa3743a5faa0c3a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+HappyMandiCms::Application.config.session_store :cookie_store, key: '_HappyMandiCms_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# HappyMandiCms::Application.config.session_store :active_record_store
